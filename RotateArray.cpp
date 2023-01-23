e6bbb3888cd741c3ad1454fa035226deb3cfa2bb
JayeshYadav99 <jayeshyadav528@gmail.com>
2023-01-23 23:20:35 +0530

    Create RotateArray.cpp

---
+++ RotateArray.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,k;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+        
+    }
+    cin>>k;
+    int a[k];
+    int i=0;
+    while(i!=k)
+    {
+       a[i]=arr[i];
+       i++;
+    }
+    
+    int start=0;
+    int mid=k;
+    while(mid<n)
+    {
+        arr[start]=arr[mid];
+        start++;
+        mid++;
+    }
+    
+    i=n-k;
+    int j=0;
+    
+     while(k--)
+    {
+      arr[i]=a[j];
+      
+       i++;
+       j++;
+    }
+    
+     for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+        
+    }
+
+ 
+
+    return 0;
+}
